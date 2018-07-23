76905ee1c8f3391e8fded67e52cae168d6466a38
sebastian-graeff <sebgraeff@hotmail.com>
2018-07-23 22:47:25 +0200

    fourth published version

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Sebastian Graeff'
 SITENAME = 'Sebastian Graeff'
-SITEURL = 'https://sebastian-graeff.github.io/datablog'
+SITEURL = 'https://sebastian-graeff.github.io/staticsite'
 RELATIVE_URLS = True
 
 # Paths
