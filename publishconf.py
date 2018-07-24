cd02b8ac853d231cb56c449dc2b18beff6ba3be5
Sebastian Graeff <41366273+sebastian-graeff@users.noreply.github.com>
2018-07-24 17:08:42 +0200

    Final publish for version 1

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://sebastian-graeff.github.io'
+SITEURL = 'https://sebastian-graeff.github.io/staticsite'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
